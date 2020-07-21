2401cbac7ba95d70d13995b4979229c1f191f685
Prajyot AKradwar <prajyot.akradwar.78@gmail.com>
2020-07-21 20:51:00 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Mubi-radarr-list
+
+<p align="left">
+	<img src="https://img.shields.io/badge/License-MIT-green?style=for-the-badge">
+</p>
+
+Scrapes [Mubi](https://www.mubi.com/showing) for movies and produces a StevenLu type list for radarr.
+> This was inspired by Steven Lu's list (https://github.com/sjlu/popular-movies).
+
+Uses beautifulsoup4 library for scraping and omdbapi library for movie metadata
+
+# Radarr
+This list uses a structure that is compatible with the StevenLuImport (https://github.com/Radarr/Radarr/wiki/Supported-NetImports#stevenluimport).
+so you can just plug this list in the url field.
+
+# Open Movie Database ([OMDb](https://www.omdbapi.com))
+
+Uses the OMDB api, so needs a api key.
+[genrate a key.](https://www.omdbapi.com/apikey.aspx)
+
+# Dependencies
+1. [omdbapi](https://pypi.org/project/omdbapi/)
+2. [tqdm](https://pypi.org/project/tqdm/)
+3. [beautifulsoup4](https://pypi.org/project/beautifulsoup4/)
+4. [requests](https://pypi.org/project/requests/)
+
+ or just
+```
+pip install requirements.txt
+```
+ also python version greater than 3.8
+
+# Initializing
+
+just paste your OMDB api in main.py
+
+# FYI
+This is my first ever usefull (and working) script, so it's sloppy and slow.
---
+++ main.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+from omdbapi.movie_search import GetMovie
+from tqdm import tqdm
+
+url = "https://mubi.com/showing"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+omdb_api_key = 'your api key'
+
+movie_title = []
+movies = []
+#movie_period = []
+
+
+division = soup.findAll('div', {'class': 'css-14yfd4f e1n7piqx4'})
+for heading in division:
+    movie_name = heading.find('h2')
+    movie_year = heading.find("span", {"class":"css-0 e1n7piqx10"})
+    movie_title.append(movie_name.text.title())
+    #movie_period.append(movie_year.tex[-4:])
+
+division2 = soup.findAll('div', {'class': 'css-14yfd4f etvj1lh3'})
+for heading2 in division2:
+    movie_name2 = heading2.find('h2')
+    movie_year2 = heading2.find("span", {"class":"css-0 etvj1lh8"})       
+    movie_title.append(movie_name2.text.title())
+    #movie_period.append(movie_year2.text[-4:])
+
+#querying omdb 
+for i in tqdm(movie_title):
+    movie_search = GetMovie(title=i, api_key=omdb_api_key)
+    results = movie_search.get_data('Title','Released', 'imdbID')
+    results['imdb_id'] = results.pop('imdbID')
+    results['title'] = results.pop('Title')
+    results['year'] = results.pop('Released')
+    movies.append(results)
+
+with open('listfile', 'w') as f:
+    
+    f.write('\u005B')
+        
+    for items in movies:
+        f.write('%s' % items + '\u002C' + '\n')
+
+with open('listfile', 'a') as file:
+    file.write('\u005D')    
